3e02c62f5cca021c766cbaa9954ecaba5b7d00f1
Allan H <allanhieng1996@gmail.com>
2017-08-04 13:15:23 -0400

    Ver.0
    -Created the repository for the 'AndroidPortfolio' android application.
    -Contains an empty activity with 'Hello World' TextView

---
+++ AndroidPortfolio/app/src/main/java/io/github/gat_x303/androidportfolio/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.gat_x303.androidportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
