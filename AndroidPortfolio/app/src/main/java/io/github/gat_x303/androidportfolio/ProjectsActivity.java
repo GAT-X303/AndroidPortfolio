4c8a9407d2e8adcde248e8f60865086177deab32
Allan H <allanhieng1996@gmail.com>
2017-08-18 12:06:49 -0400

    Version 1.0
    -Fixed minor issue with Projects and added more projects
    -More minor visuals to come

--- AndroidPortfolio/app/src/main/java/io/github/gat_x303/androidportfolio/ProjectsActivity.java
+++ AndroidPortfolio/app/src/main/java/io/github/gat_x303/androidportfolio/ProjectsActivity.java
@@ -18,12 +18,22 @@ public class ProjectsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
 
-        String title1 = "Analog to Digital Converter (ADC)";
+        String title1 = "Analog to Digital Converter";
         String info1 = "To summarize this was about applying my knowledge of microprocessor systems that largely involved learning through the EsduinoXtreme. The microprocessor was coded in Assembly through CodeWarrior and connected with a USBDM to be programmed. The project required a circuit that took an input scaled it, and converted it to digital values to be displayed using Matlab.\n\nSkills Applied\n-Circuit Design\n-Assembly\n-Matlab";
         String address1 = "https://gat-x303.github.io/project1.html";
 
+        String title2 = "My Portfolio Site";
+        String info2 = "Apply my knowledge, in HTML/CSS and web design to create a website that will act as my online portfolio that will updated as I acquiring skills and complete projects.";
+        String address2 = "https://gat-x303.github.io/project2.html";
+
+        String title3 = "Android Portfolio";
+        String info3 = "To design an Android application and apply my skills of basic Android development that would contain similar information to my portfolio site.";
+        String address3 = "https://gat-x303.github.io/project3.html";
+
         ArrayList<Item> projectsList = new ArrayList<>();
         projectsList.add(new Item(title1, info1, address1));
+        projectsList.add(new Item(title2, info2, address2));
+        projectsList.add(new Item(title3, info3, address3));
 
         //creates RecyclerView along with a LinearLayoutManager, using the 2 allows use to recycle CardViews much like ArrayAdapter
         RecyclerView recList = (RecyclerView) findViewById(R.id.recyclerView);
